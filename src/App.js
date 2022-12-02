b877ccc320c16e504cebff237a9ec2630a5a6be9
Ana Sousa <anasousa314@outlook.com>
2022-12-02 09:33:59 -0300

    aula33-Manipulando o lifecycle com o useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useMemo} from 'react';
+import React,{useState,useMemo, useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -8,6 +8,7 @@ import themes from './styles/themes';
 
 function App() {
   const [theme, setTheme] = useState('dark');
+  // const [qualquer, setQualquer] = useState(123)
 
   const currentTheme = useMemo(()=>{
     return themes[theme] || themes.dark;
@@ -22,10 +23,21 @@ function App() {
       :'dark')
   }
 
+  // função de efeito
+  // sempre q hover uma renderização o useEffect vai rodar
+  useEffect(() => {
+    console.debug('useEffect executou')
+    localStorage.setItem('theme', JSON.stringify(theme))
+  },[theme])//para executar apenas uma vez depois do mount, é só colocar esse array de dependencias vazio
+  // com esse array vazio o useEffect só será executado uma vez
+  // com algum elemnto dentro do array ele executará toda vez que houver uma alteração no state ou na prop q estiver no array
+
+  // console.debug(qualquer)
   return (
     <>
       <ThemeProvider theme={currentTheme}>
         <GlobalStyle />
+        {/* <button onClick={() => setQualquer(Math.random())}>Olá</button> */}
         <Layout 
           onToggleTheme={handleToogleTheme}
           selectedTheme={theme}
