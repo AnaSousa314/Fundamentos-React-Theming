dc364643d92e6a3430ac536959e038c9bae439e1
Ana Sousa <anasousa314@outlook.com>
2022-12-02 10:21:19 -0300

    aula35-diferencas entre o useEffect e o useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useMemo, useEffect} from 'react';
+import React,{useState,useMemo, useEffect, useLayoutEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -25,6 +25,7 @@ function App() {
 
   // função de efeito
   // sempre q hover uma renderização o useEffect vai rodar
+  //ele ASSÍNCRONA
   // useEffect(() => {
   //   console.debug('useEffect executou')
   //   localStorage.setItem('theme', JSON.stringify(theme))
@@ -33,18 +34,49 @@ function App() {
   // com algum elemnto dentro do array ele executará toda vez que houver uma alteração no state ou na prop q estiver no array
 
   // console.debug(qualquer)
+
+  // esse é executado antes do react jogar as mudanças na tela do usuário. ele é SÍNCRONO, ele trava a renderização
+  useLayoutEffect(() =>{
+    console.log('useLayoutEffect');
+
+    for (let i = 0; i <= 15000; i++) {
+      console.debug(i)
+      
+    }
+  },[theme]) 
+
+  // useEffect(() => {
+  //     console.debug('tema mudou')
+  //     for (let i = 0; i <= 15000; i++) {
+  //       console.debug(i + 'useEffect')
+        
+  //     }
+  //   },[theme])
+
+    // useEffect(() => {
+    //   console.debug('primeiro render')
+    // },[])
+
+    // useEffect(() => {
+    //   console.debug('todo render')
+    // })
+
   return (
     <>
       <ThemeProvider theme={currentTheme}>
         <GlobalStyle />
         {/* <button onClick={() => setQualquer(Math.random())}>Olá</button> */}
-        <button onClick={handleToogleTheme}>toggle</button>
-        {theme === 'dark' && (
+        {/* <button onClick={handleToogleTheme}>toggle</button> */}
+        {/* {theme === 'dark' && (
           <Layout 
             onToggleTheme={handleToogleTheme}
             selectedTheme={theme}
           />
-        )}
+        )} */}
+        <Layout 
+          onToggleTheme={handleToogleTheme}
+          selectedTheme={theme}
+        />
       </ThemeProvider>
     </>
   );
